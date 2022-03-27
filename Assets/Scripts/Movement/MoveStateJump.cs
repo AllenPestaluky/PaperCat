2ca22dd6fb13c13a915818f5a52782f62ad14fd9
Martin Forget <martforg@hotmail.com>
2022-03-27 15:44:06 -0400

    Changed the implementation of the jump state.

--- Assets/Scripts/Movement/MoveStateJump.cs
+++ Assets/Scripts/Movement/MoveStateJump.cs
@@ -5,12 +5,21 @@ using UnityEngine.InputSystem;
 
 public class MoveStateJump : MoveStateBase
 {
+    public float TapJumpDistance = 4f;
+    public float TapJumpHeigh = 4f;
+
     Rigidbody m_RigidBody;
+    LineRenderer m_ArcRenderer;
+
+    float m_Time = 0f;
+    JumpArc m_JumpArc;
 
     public new void Start()
     {
         base.Start();
         m_RigidBody = GetComponent<Rigidbody>();
+        m_ArcRenderer = GetComponent<LineRenderer>();
+        HideJumpArc();
     }
 
     public override CatMovement.EMoveState GetStateEnum()
@@ -18,10 +27,38 @@ public class MoveStateJump : MoveStateBase
         return CatMovement.EMoveState.Jump;
     }
 
+    public override void Enter()
+    {
+        m_Time = 0f;
+        m_JumpArc = new JumpArc(transform.position, transform.position + transform.forward * TapJumpDistance, TapJumpHeigh, Physics.gravity);
+        ShowJumpArc(m_JumpArc);
+    }
+
+    public override void ActiveStateFixedUpdate(float deltaTime)
+    {
+        m_Time += deltaTime;
+    }
+
     public override void OnJumpActionCanceled() 
     {
-        //Debug.Log("MoveStateBase Action Canceled");
-        Vector3 jumpForce = new Vector3(0.0f, 25.0f, 0.0f);
-        m_RigidBody.AddForce(jumpForce, ForceMode.Impulse);
+        m_RigidBody.AddForce(m_JumpArc.InitialVelocity, ForceMode.VelocityChange);
+    }
+
+    void ShowJumpArc(JumpArc arc)
+    {
+        int segments = m_ArcRenderer.positionCount;
+        Vector3[] positions = new Vector3[segments];
+        float tf = arc.tf;
+        for (int i = 0; i < segments; i++)
+        {
+            positions[i] = arc.GetPositionAlong(i * tf / (segments - 1));
+        }
+        m_ArcRenderer.SetPositions(positions);
+        m_ArcRenderer.enabled = true;
+    }
+
+    void HideJumpArc()
+    {
+        m_ArcRenderer.enabled = false;
     }
 }
